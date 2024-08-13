9bff6acc275df01ee7a5072cacb57bcf2130d643
Nico Felbinger <nico@felbinger.eu>
2024-08-13 19:47:46 +0200

    Django admin: Set title and header

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,3 +21,6 @@ from django.urls import path
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+admin.site.site_header = admin.site.site_title = "Django example"
+admin.site.index_title = "Welcome to Django example"
